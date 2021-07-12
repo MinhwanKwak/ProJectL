1d16da32139165db0e52f0f42dbaff01d38269c3
moonjyo <m_jyo@naver.com>
2021-07-12 23:26:30 +0900

    vertical recoil test

--- 2021CK_Project/Assets/Scripts/PlayerShot.cs
+++ 2021CK_Project/Assets/Scripts/PlayerShot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerShot : MonoBehaviour
 {
@@ -9,8 +10,19 @@ public class PlayerShot : MonoBehaviour
     int AmmoCount;
     int AmmoMaxCount;
 
+    GameObject CamPivot;
+
+    public void Start()
+    {
+        CamPivot = PlayerManager.Instance.playerMove.CamPivot;
+    }
+
     public void Shot()
     {
+        CamPivot.transform.DOLocalRotate(CamPivot.transform.localEulerAngles - new Vector3(1, 0, 0), 0.2f).OnComplete(() => CamPivot.transform.DOLocalRotate(CamPivot.transform.localEulerAngles + new Vector3(1, 0, 0), 0.2f));
+        
+
+
         Aim = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
         Ray ray = Camera.main.ScreenPointToRay(Aim);
         if(Physics.Raycast(ray, out RaycastHit hit))
