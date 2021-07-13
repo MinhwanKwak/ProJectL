747443e7cc8581f8285c07de1562095918849e81
moonjyo <m_jyo@naver.com>
2021-07-13 14:05:03 +0900

    vertical recoil back test

--- 2021CK_Project/Assets/Scripts/PlayerShot.cs
+++ 2021CK_Project/Assets/Scripts/PlayerShot.cs
@@ -12,23 +12,69 @@ public class PlayerShot : MonoBehaviour
 
     GameObject CamPivot;
 
+    bool T = false;
+    float Times = 0;
+
+    float RecoilTimes = 0.1f;
+
     public void Start()
     {
         CamPivot = PlayerManager.Instance.playerMove.CamPivot;
     }
 
+    public void Update()
+    {
+        //if(T)
+        //{
+        //    Times += Time.deltaTime;
+        //    CamPivot.transform.localEulerAngles = Vector3.Lerp(CamPivot.transform.localEulerAngles, CamPivot.transform.localEulerAngles - new Vector3(15, 0, 0), Time.deltaTime* 2);
+        //}
+
+        //if(Times > 0.2f)
+        //{
+        //    T = false;
+        //    Times = 0;
+        //}
+    }
+
     public void Shot()
     {
-        CamPivot.transform.DOLocalRotate(CamPivot.transform.localEulerAngles - new Vector3(1, 0, 0), 0.2f).OnComplete(() => CamPivot.transform.DOLocalRotate(CamPivot.transform.localEulerAngles + new Vector3(1, 0, 0), 0.2f));
-        
-
-
         Aim = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
         Ray ray = Camera.main.ScreenPointToRay(Aim);
-        if(Physics.Raycast(ray, out RaycastHit hit))
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
             Debug.Log(hit.collider.name);
         }
-        
+        //CamPivot.transform.DOLocalRotate(CamPivot.transform.localEulerAngles - new Vector3(5, 0, 0), 0.2f);//.OnComplete(() => CamPivot.transform.DOLocalRotate(CamPivot.transform.localEulerAngles + new Vector3(1, 0, 0), 0.2f));
+        Times = 0;
+        StartCoroutine(VerticalRecoil());        
+        //T = true;
+
+
+    }
+
+    IEnumerator VerticalRecoil()
+    {        
+        while(Times < RecoilTimes)
+        {
+            Times += Time.deltaTime;
+            CamPivot.transform.localEulerAngles = Vector3.Lerp(CamPivot.transform.localEulerAngles, CamPivot.transform.localEulerAngles - new Vector3(15, 0, 0), Time.deltaTime*5);
+
+            yield return null;
+        }
+
+        StartCoroutine(ReturnAim());
+    }
+
+    IEnumerator ReturnAim()
+    {
+        Times = 0;
+        while (Times < RecoilTimes)
+        {
+            Times += Time.deltaTime;
+            CamPivot.transform.localEulerAngles = Vector3.Lerp(CamPivot.transform.localEulerAngles, CamPivot.transform.localEulerAngles + new Vector3(15, 0, 0), Time.deltaTime * 5);
+
+            yield return null;
+        }
     }
 }
